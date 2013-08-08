95cf550cac9db35e68f3cf8e6d2b9cb7768a07ea
Ronald Luitwieler (MacBookie Pro) <ronald.luitwieler@gmail.com>
2013-08-08 15:10:32 +0200

    Initial commit

---
+++ make.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+############################
+#  http://blog.smalleycreative.com/tutorials/using-git-and-github-to-manage-your-dotfiles/
+# .make.sh
+# This script creates symlinks from the home directory to any desired dotfiles in ~/dotfiles
+############################
+
+########## Variables
+
+# dotfiles directory
+dir=~/dotfiles       
+# list of files/folders to symlink in homedir
+symlink_files="aliasses functions profile"    
+
+##########
+
+echo "Changing to the $dir directory"
+cd $dir
+
+echo "Then create symlinks"  
+for file in $symlink_files; do
+    echo "Remove .$file"
+    rm ~/.$file
+    echo "Creating symlink to $file in home directory."
+    ln -s $dir/$file ~/.$file
+done
+
+
+echo "...done"
---
+++ profile
@@ -0,0 +1,7 @@
+source ~/.aliasses
+source ~/.functions
+
+export PS1="\u@\h:\w
+$ "
+
+
