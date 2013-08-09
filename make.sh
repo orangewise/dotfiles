e53f39bf8d891a2af0a02c0337938fbc6e53c68c
Ronald Luitwieler (MacBookie Pro) <ronald.luitwieler@gmail.com>
2013-08-09 12:22:04 +0200

    Custom profiles for each computer

--- make.sh
+++ make.sh
@@ -5,25 +5,50 @@
 # This script creates symlinks from the home directory to any desired dotfiles in ~/dotfiles
 ############################
 
+########## Functions
+
+function symlink {
+    echo "Remove symlink .$1"
+    rm ~/.$1
+    echo "Creating symlink ~/.$1 to $2"
+    ln -s $dir/$2 ~/.$1
+}
+
+
 ########## Variables
 
 # dotfiles directory
 dir=~/dotfiles       
 # list of files/folders to symlink in homedir
-symlink_files="aliases functions git-prompt profile"    
+symlink_files="aliases functions git-prompt gitprompt"  
 
-##########
+########## Start
 
 echo "Changing to the $dir directory"
 cd $dir
 
+
 echo "Then create symlinks"  
 for file in $symlink_files; do
-    echo "Remove .$file"
-    rm ~/.$file
-    echo "Creating symlink to $file in home directory."
-    ln -s $dir/$file ~/.$file
+	
+	symlink $file $file
+	
 done
+
+echo "Create the correct profile"  
+if [ `hostname` == "Ro-MacBookie-Pro.local" ]
+then
+	echo "--> profile for Ro-Mac-BookiePro"
+	symlink "profile" "profile_ro_macbookie_pro"
+	
+elif [ hostname == "Ro-MacBook-Pro.local" ]
+then
+	echo "--> profile for Ro-MacBook-Pro"
+	symlink "profile" "profile_ro_macbook_pro"
+
+else
+	echo "--> profile not created..."
+fi
 
 
 echo "...done"
